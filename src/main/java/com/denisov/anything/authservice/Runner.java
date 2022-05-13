abc2ddaf7bed8ff4960cccb3a26efc9e5b13478e
YarO0s <yar.d.7772525@gmail.com>
2022-05-13 22:15:05 +0300

    herokuanything10

--- src/main/java/com/denisov/anything/authservice/Runner.java
+++ src/main/java/com/denisov/anything/authservice/Runner.java
@@ -29,7 +29,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
                         "com.denisov.anything.productset","com.denisov.anything.steps"})
 
 @EntityScan({"com.denisov.anything.products","com.denisov.anything.steps.RecipeEntity.class","com.denisov.anything.",
-             "com.denisov.anything.authservice.user", "com.denisov.anything.authservice.token",
+             "com.denisov.anything.authservice.user",
              "com.denisov.anything.steps", "com.denisov.anything.productset"})
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class })
 public class Runner {
